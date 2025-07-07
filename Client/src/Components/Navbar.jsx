2e9ffda92aedf49b59d54fbf890b653306af041d
Uddeshya Rawat <uddeshyarawat4@gmail.com>
2025-07-07 21:46:23 +0530

    add feature section

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
 
 
             {/* navbar links  
-            max-sm menas width smaller than 40rem that is mobile view */}
+            max-sm means width smaller than 40rem that is mobile view */}
 
             <div className={`max-sm:fixed  max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col sm:flex-row items-start sm:items-center gap-4 sm:gap-8 
                   max-sm:p-4 transition-all duration-300 z-50
@@ -65,6 +65,8 @@ const Navbar = () => {
             </button>
 
         </div>
+
+      
     )
 }
 
